aad821d95828154432d4ad4141474fd5c49014f6
unknown <missonpkmn@gmail.com>
2021-06-09 00:29:45 -0400

    Add clientId indices to new entities

--- server/src/shared/infra/db/entities/auth-code.entity.ts
+++ server/src/shared/infra/db/entities/auth-code.entity.ts
@@ -1,4 +1,4 @@
-import { AfterCreate, Entity, Property } from '@mikro-orm/core'
+import { AfterCreate, Entity, Property, Index } from '@mikro-orm/core'
 import { DomainEvents } from '../../../domain/events/domain-events'
 import { UniqueEntityID } from '../../../domain/unique-entity-id'
 import { BaseEntity } from './base.entity'
@@ -6,6 +6,7 @@ import { BaseEntity } from './base.entity'
 @Entity()
 export class AuthCodeEntity extends BaseEntity {
   @Property()
+  @Index()
   clientId!: string
 
   @Property()
--- server/src/shared/infra/db/entities/auth-secret.entity.ts
+++ server/src/shared/infra/db/entities/auth-secret.entity.ts
@@ -1,9 +1,10 @@
-import { Entity, Property } from '@mikro-orm/core'
+import { Entity, Property, Index } from '@mikro-orm/core'
 import { BaseEntity } from './base.entity'
 
 @Entity()
 export class AuthSecretEntity extends BaseEntity {
   @Property()
+  @Index()
   clientId!: string
 
   @Property()
