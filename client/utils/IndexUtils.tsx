27a953502174e8f53cf17e3b17f26384c5770ef0
KTong821 <kevintong0821@gmail.com>
2021-06-19 13:45:07 -0400

    Removed client & scripts code

--- client/utils/IndexUtils.tsx
+++
@@ -1,3 +0,0 @@
-export const indexData = <K, V extends { id: K }>(items: Array<V> = []): Map<K, V> => {
-  return new Map<K, V>(items.map((item) => [item.id, item]))
-}
--- client/utils/serde/index.ts
+++
@@ -1,1 +0,0 @@
-export { deserializeClubsAndEvents } from './clubs-and-events'
