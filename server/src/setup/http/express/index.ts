14336e290e326e2a9eeb651ae5cf033b91135479
unknown <missonpkmn@gmail.com>
2021-08-02 23:50:01 -0400

    Remove redundant variable naming

--- server/src/setup/http/express/index.ts
+++ server/src/setup/http/express/index.ts
@@ -1,4 +1,4 @@
-export { APIRouter, WebServer, AuthExpress as AuthExpress } from './types'
+export { APIRouter, WebServer, AuthExpress } from './types'
 export { setupVersionedAPIRouter } from './versioned-api-router'
 export { BasicWebServerOptions, setupBasicWebServer } from './basic-web-server'
 export { AuthExpressOptions, setupAuthExpress } from './auth-express'
